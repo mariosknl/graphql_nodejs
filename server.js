1b54b1047fdd5be9b059a7a90b7b50b65aec05e4
mariosknl <mariosknl@outlook.com>
2024-02-21 10:41:53 +0200

    object types

--- server.js
+++ server.js
@@ -1,39 +1,103 @@
-var { graphql, buildSchema } = require("graphql");
+var {
+  graphql,
+  buildSchema,
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+} = require("graphql");
 var express = require("express");
 var { createHandler } = require("graphql-http/lib/use/express");
 var { ruruHTML } = require("ruru/server");
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello(name: String!): String
+// var schema = buildSchema(`
+//   type Query {
+//     hello(name: String!): String
 
-    age: Int
-    weight: Float
-    isOver18: Boolean
+//     age: Int
+//     weight: Float
+//     isOver18: Boolean
+//     hobbies: [String!]!
 
-    hobbies: [String!]!
-  }
-`);
+//     user: User
+//   }
+
+//   type User {
+//     id: Int
+//     name: String
+//   }
+// `);
+
+const User = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: {
+      type: GraphQLInt,
+    },
+    name: {
+      type: GraphQLString,
+      resolve: (obj) => {
+        const name = obj.name.trim().toUpperCase();
+        if (obj.isAdmin) {
+          return `${name} (Admin)`;
+        }
+        return name;
+      },
+    },
+  },
+});
+
+// const schema = new GraphQLSchema({
+//   query: new GraphQLObjectType({
+//     name: "Query",
+//     fields: {
+//       hello: {
+//         type: GraphQLString,
+//         resolve: () => {
+//           return "Hello World";
+//         },
+//       },
+
+//       user: {
+//         type: User,
+//         resolve: () => {
+//           return {
+//             id: 1,
+//             name: "John Doe",
+//             isAdmin: true,
+//           };
+//         },
+//       },
+//     },
+//   }),
+// });
 
 // The rootValue provides a resolver function for each API endpoint
-var rootValue = {
-  hello: ({ name }) => {
-    return `Hello ${name}`;
-  },
-  age: () => {
-    return 36;
-  },
-  weight: () => {
-    return 75.5;
-  },
-  isOver18: () => {
-    return true;
-  },
-  hobbies: () => {
-    return ["Reading", "Gardening", "Cooking"];
-  },
-};
+// var rootValue = {
+//   hello: ({ name }) => {
+//     return `Hello ${name}`;
+//   },
+//   age: () => {
+//     return 36;
+//   },
+//   weight: () => {
+//     return 75.5;
+//   },
+//   isOver18: () => {
+//     return true;
+//   },
+//   hobbies: () => {
+//     return ["Reading", "Gardening", "Cooking"];
+//   },
+
+//   user: () => {
+//     return {
+//       id: 1,
+//       name: "John Doe",
+//     };
+//   },
+// };
 
 const app = express();
 
@@ -41,7 +105,7 @@ app.all(
   "/graphql",
   createHandler({
     schema,
-    rootValue,
+    // rootValue,
   })
 );
 
