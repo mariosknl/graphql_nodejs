5c08b8b5967188f73a5d0acf4f026605d2deaeb5
mariosknl <mariosknl@outlook.com>
2024-02-21 09:57:37 +0200

    add ruru GraphiQL interface

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 var { graphql, buildSchema } = require("graphql");
+var express = require("express");
+var { createHandler } = require("graphql-http/lib/use/express");
+var { ruruHTML } = require("ruru/server");
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -18,11 +21,22 @@ var rootValue = {
   },
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: "{ age }",
-  rootValue,
-}).then((response) => {
-  console.log(response);
+const app = express();
+
+app.all(
+  "/graphql",
+  createHandler({
+    schema,
+    rootValue,
+  })
+);
+
+// Serve the GraphiQL IDE.
+app.get("/", (_req, res) => {
+  res.type("html");
+  res.end(ruruHTML({ endpoint: "/graphql" }));
 });
+
+// Start the server at port
+app.listen(4000);
+console.log("Running a GraphQL API server at http://localhost:4000/graphql");
